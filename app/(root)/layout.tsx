97b11644cb967c98a98121222ab7b091b580cd2c
John Pamisi <pamisijohn@gmail.com>
2024-10-10 17:25:11 +0100

    homepage layout with sidebar and aside

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,27 @@
+import MobileNav from "@/components/MobileNav";
+import SideBar from "@/components/Sidebar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Adrian", lastName: "Pamisi" };
+
   return (
-    <main>
-      SIDEBAR
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <SideBar user={loggedIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" alt="logo" width={30} height={30} />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
